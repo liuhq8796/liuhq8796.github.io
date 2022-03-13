716dee908700b3cf22361516557d2160266bbdcb
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-03-13 16:01:33 +0800

    迁移 bookmarks 数据

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/bookmarks",
+      name: "bookmarks",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/BookmarksView.vue"),
     },
   ],
 });
---
+++ src/views/BookmarksView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import BookmarksJson from "@/json/bookmarks.json";
+</script>
+
+<template>
+  <main class="bookmarks">
+    <h1>This is an bookmarks page</h1>
+
+    <ul>
+      <li v-for="(item, index) of BookmarksJson" :key="index" class="card">
+        <div class="card__header">{{ item.name }}</div>
+        <div class="card__body">
+          <div v-for="(link, index) of item.children" :key="index">
+            <a :href="link.href">
+              <img v-if="link.icon" :src="link.icon" :alt="link.name" />
+              {{ link.name }}
+            </a>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .bookmarks {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
