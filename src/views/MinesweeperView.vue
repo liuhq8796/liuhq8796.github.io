bc3054656d1b35867dcf6227180941ed0af67546
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-04-05 17:46:07 +0800

    写个扫雷 (3/9)

--- src/views/MinesweeperView.vue
+++ src/views/MinesweeperView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, watchEffect } from "vue";
+
+// Components
+import IconBomb from "../components/icons/IconBomb.vue";
+import IconFlag from "../components/icons/IconFlag.vue";
 
 interface BlockState {
   x: number;
@@ -10,8 +14,8 @@ interface BlockState {
   flagged?: boolean;
 }
 
-const WIDTH = 10;
-const HEIGHT = 10;
+const WIDTH = 5;
+const HEIGHT = 5;
 const data = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from(
@@ -20,24 +24,46 @@ const data = reactive(
         x,
         y,
         adjacentMines: 0,
+        revealed: false,
       })
     )
   )
 );
 
-const onClick = (x: number, y: number) => {
-  console.log(x, y);
+let mineGenerated = false;
+let dev = false;
+
+const onClick = (item: BlockState) => {
+  if (!mineGenerated) {
+    generateMines(item);
+    updateNumbers();
+    mineGenerated = true;
+  }
+
+  item.revealed = true;
+  if (item.mine) {
+    alert("BOOM!");
+  } else {
+    expendZero(item);
+  }
 };
 
-const generateMines = () => {
+const onRightClick = (block: BlockState) => {
+  block.flagged = !block.flagged;
+};
+
+const generateMines = (initial: BlockState) => {
   for (const row of data) {
     for (const block of row) {
+      if (
+        Math.abs(initial.x - block.x) <= 1 &&
+        Math.abs(initial.y - block.y) <= 1
+      )
+        continue;
       block.mine = Math.random() < 0.1;
     }
   }
 };
-
-generateMines();
 
 const DIRECTIONS = [
   [-1, -1],
@@ -50,27 +76,71 @@ const DIRECTIONS = [
   [-1, 0],
 ] as const;
 
+const NUMBER_COLOR = [
+  "text-transparent",
+  "text-blue-500",
+  "text-green-500",
+  "text-red-500",
+  "text-orange-500",
+  "text-purple-500",
+  "text-pink-500",
+  "text-yellow-500",
+  "text-gray-500",
+];
+
 const updateNumbers = () => {
   for (const row of data) {
     for (const item of row) {
       if (item.mine) continue;
-      for (const [dx, dy] of DIRECTIONS) {
-        const x = item.x + dx;
-        const y = item.y + dy;
-        if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) continue;
-        if (data[y][x].mine) {
-          item.adjacentMines++;
-        }
-      }
+      getSibilings(item).forEach((sibiling) => {
+        if (sibiling.mine) item.adjacentMines++;
+      });
     }
   }
 };
 
-updateNumbers();
-
 const getBlockClass = (item: BlockState) => {
-  return item.mine ? "text-red-400" : "text-gray-400";
+  if (!item.revealed) return "bg-gray-400/10 hover:bg-gray-400";
+  return item.mine ? "text-red-400" : NUMBER_COLOR[item.adjacentMines];
 };
+
+const expendZero = (block: BlockState) => {
+  if (block.adjacentMines > 0) return;
+  getSibilings(block).forEach((sibiling) => {
+    if (sibiling.flagged || sibiling.revealed) return;
+    sibiling.revealed = true;
+    expendZero(sibiling);
+  });
+};
+
+const getSibilings = (block: BlockState) => {
+  const sibilings = [] as BlockState[];
+  for (const [dx, dy] of DIRECTIONS) {
+    const x = block.x + dx;
+    const y = block.y + dy;
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) continue;
+    sibilings.push(data[y][x]);
+  }
+  return sibilings;
+};
+
+const flat = (arr: BlockState[][]) => {
+  return arr.reduce((acc, val) => acc.concat(val), []);
+};
+
+const checkGameState = () => {
+  const blocks = flat(data);
+
+  if (blocks.every((block) => block.revealed || block.flagged)) {
+    if (blocks.every((block) => block.flagged && block.mine)) {
+      alert("You cheat!");
+    } else {
+      alert("You win!");
+    }
+  }
+};
+
+watchEffect(checkGameState);
 </script>
 
 <template>
@@ -78,16 +148,22 @@ const getBlockClass = (item: BlockState) => {
     <div>
       <h1 class="text-center">Minesweeper</h1>
       <div class="mt-2">
-        <div v-for="(row, y) in data" :key="y">
+        <div v-for="(row, y) in data" :key="y" class="flex">
           <button
-            class="w-10 h-10 border hover:bg-gray-400"
+            class="w-10 h-10 border border-gray-400 flex justify-center items-center"
             :class="getBlockClass(item)"
             v-for="(item, x) in row"
             :key="x"
-            @click="onClick(x, y)"
+            @click="onClick(item)"
+            @contextmenu.prevent="onRightClick(item)"
           >
-            <div v-if="item.mine">x</div>
-            <div v-else>{{ item.adjacentMines }}</div>
+            <template v-if="item.flagged"><IconFlag /></template>
+            <template v-else-if="item.revealed || dev">
+              <div v-if="item.mine">
+                <IconBomb />
+              </div>
+              <div v-else>{{ item.adjacentMines }}</div>
+            </template>
           </button>
         </div>
       </div>
