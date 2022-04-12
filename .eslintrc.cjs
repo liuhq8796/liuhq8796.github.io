e2dfddab465c8793039efb53ac4c6864dc45795a
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-04-12 14:33:47 +0800

    安装并配置 unplugin-auto-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "./.eslintrc-auto-imports.js",
   ],
   env: {
     "vue/setup-compiler-macros": true,
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ["vue", "vue-router", "@vueuse/core", "pinia"],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
