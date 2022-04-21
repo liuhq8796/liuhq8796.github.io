08b547710ae1820470bd3f846faf75d69db07f1e
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-04-22 00:41:29 +0800

    chore: 安装并配置 rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import Inspect from "vite-plugin-inspect";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,8 @@ export default defineConfig({
       dts: true,
     }),
     Inspect(),
+    // put it the last one
+    visualizer(),
   ],
   resolve: {
     alias: {
