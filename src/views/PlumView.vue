f81b7d7470f511bab9e74f70fb30779c2939dbba
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2022-05-06 15:11:29 +0800

    更新 Plum 添加注释

--- src/views/PlumView.vue
+++ src/views/PlumView.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 // Interface
-interface Point {
-  x: number;
-  y: number;
-}
-
-interface Branch {
-  start: Point;
-  angle: number;
-  length: number;
-}
+import type { Point, Branch } from "@/types/Plum";
 
 // canvas 宽度
 let width = 0;
@@ -30,9 +21,12 @@ const ctx = ref<CanvasRenderingContext2D | null>(null);
 const branchRenderQueue: (() => void)[] = [];
 
 onMounted(() => {
-  el.value && (ctx.value = setupCanvas(el.value));
-  ctx.value && (ctx.value.strokeStyle = COLOR);
+  // 初始化 canvas
+  ctx.value = setupCanvas(el.value!);
+  // 设置画笔颜色
+  ctx.value.strokeStyle = COLOR;
 
+  // 向队列中添加第一个递归渲染分支函数
   branchRenderQueue.push(() => {
     branchLoop({
       start: { x: width / 2, y: height },
@@ -40,6 +34,7 @@ onMounted(() => {
       length: length.value,
     });
   });
+  // 启动渲染分支函数队列
   renderStart();
 });
 
@@ -61,76 +56,100 @@ const setupCanvas = (canvas: HTMLCanvasElement) => {
   const ctx = canvas.getContext("2d");
   // Scale all drawing operations by the dpr, so you
   // don't have to worry about the difference.
-  ctx && ctx.scale(dpr, dpr);
+  ctx!.scale(dpr, dpr);
 
-  return ctx;
+  return ctx!;
 };
 
+// 递归渲染分支函数
 const branchLoop = (b: Branch, depth = 0) => {
+  // 渲染分支并获取结束位置作为下一个分支起点
   const end = drawBranch(b);
   // 超出画布外的分支不再渲染
   if (end.x < 0 || end.x > width || end.y < 0 || end.y > height) return;
 
+  // 如果深度小于最小深度则必定执行，否则按概率执行
   if (depth < minDepth.value || Math.random() < 0.5) {
+    // 设置左分支
     const leftBranch: Branch = {
       start: end,
       length: b.length,
       angle: b.angle - (Math.random() * Math.PI) / 6,
     };
+    // 将左分支渲染函数添加到队列中，并将深度加1
     branchRenderQueue.push(() => branchLoop(leftBranch, depth + 1));
   }
 
+  // 如果深度小于最小深度则必定执行，否则按概率执行
   if (depth < minDepth.value || Math.random() < 0.5) {
+    // 设置右分支
     const rightBranch: Branch = {
       start: end,
       length: b.length,
-      angle: b.angle + (Math.random() * 0.9 * Math.PI) / 6,
+      angle: b.angle + (Math.random() * Math.PI) / 6,
     };
+    // 将右分支渲染函数添加到队列中，并将深度加1
     branchRenderQueue.push(() => branchLoop(rightBranch, depth + 1));
   }
 };
 
+// 上一次执行时间
 let lastTime = 0;
 
+// 启动渲染分支函数队列
 const renderStart = () => {
   requestAnimationFrame((time) => {
+    // 如果距离上一次执行时间小于 16ms 则不执行
+    // 统一高刷新率屏幕动画间隔
     if (time - lastTime > 16) {
+      // 更新上一次执行时间
       lastTime = time;
+      // 浅拷贝队列
       const tempQueue = [...branchRenderQueue];
+      // 清空队列
       branchRenderQueue.length = 0;
+      // 遍历执行队列中的渲染函数
       tempQueue.forEach((fn) => fn());
     }
+    // 如果队列中还有渲染函数则继续执行
     if (branchRenderQueue.length > 0) {
       renderStart();
     }
   });
 };
 
+// 绘制分支
 const drawBranch = (b: Branch) => {
   const end = getEndPoint(b);
   lineTo(b.start, end);
   return end;
 };
 
+// 获取分支结束点
 const getEndPoint = (b: Branch) => {
   const x = b.start.x + b.length * Math.cos(b.angle);
   const y = b.start.y + b.length * Math.sin(b.angle);
   return { x, y };
 };
 
+// 绘制线段
 const lineTo = (start: Point, end: Point) => {
-  ctx.value?.beginPath(); // Start a new path
-  ctx.value?.moveTo(start.x, start.y); // Move the pen to (30, 50)
-  ctx.value?.lineTo(end.x, end.y); // Draw a line to (150, 40)
-  ctx.value?.stroke(); // Render the path
+  ctx.value!.beginPath(); // Start a new path
+  ctx.value!.moveTo(start.x, start.y); // Move the pen to (30, 50)
+  ctx.value!.lineTo(end.x, end.y); // Draw a line to (150, 40)
+  ctx.value!.stroke(); // Render the path
 };
 
+// 重置画布
 const clearCanvas = () => {
-  ctx.value?.clearRect(0, 0, width, height);
+  ctx.value!.clearRect(0, 0, width, height);
 };
 
+// 重新绘制
 const repaint = () => {
+  // 清除画布
   clearCanvas();
+  // 清除渲染队列
   branchRenderQueue.length = 0;
 
   branchRenderQueue.push(() => {
