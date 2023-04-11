9107f3ad0474027449975b0874dce823a0a76efd
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-04-11 03:36:39 +0000

    chore: 安装 unplugin-auto-import 和 unplugin-vue-components 并配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), UnoCSS()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    UnoCSS(),
+    Components({
+      dts: true,
+      types: [
+        {
+          from: 'vue-router',
+          names: ['RouterLink', 'RouterView']
+        }
+      ]
+    }),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // global imports to register
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      eslintrc: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
